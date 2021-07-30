12bd9198fbeec9dfb01a3fcdf5a48e7b13356618
hudavn <ddforwork08@gmail.com>
2021-07-30 23:03:56 +0700

    Main

---
+++ object.py
@@ -0,0 +1,12 @@
+
+fileName = "input.txt"
+
+subtree = list() # Subproblem, tree decomposition
+impact = list()
+subres = list() # Res of subproblem
+order = list() # Thứ tự xử lí subproblem
+
+start = dict()
+goal = dict()
+
+StateSpace = set()
